34ce82afbf452f3e29a08526801fbc56c8334caa
Mikebulatov <mikebulatov@yandex.ru>
2023-09-05 20:00:46 +0700

    Задача 47.Изменение 1

--- zd47/Program.cs
+++ zd47/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Console.WriteLine("Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами..");
+
+
+
+Console.Write("Введите первую размерность массива:");
+int rows=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите вторую размерность массива:");
+int columns=Convert.ToInt32(Console.ReadLine());
+
+float [,] matrix=new float [rows,columns];
+
+Console.WriteLine("Сформированный массив:");
+for(int i=0; i<matrix.GetLength(0); i++)
+{
+    for(int j=0; j<matrix.GetLength(0); j++)
+    {
+    matrix[i,j]= new Random().Next(0,10);
+    matrix[i,j]= matrix[i,j] / new Random().Next(1,10);
+    Console.Write("{0:F1} ", matrix[i,j]);
+    
+    }
+    Console.WriteLine();
+}
+
