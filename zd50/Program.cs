6e5d45a692df84488b2d322bed9a48492e0807a5
Mikebulatov <mikebulatov@yandex.ru>
2023-09-05 22:22:07 +0700

    Задача 50.Изменение 1.

--- zd50/Program.cs
+++ zd50/Program.cs
@@ -1,20 +1,18 @@
 ﻿
-
 Console.WriteLine("ЗЗадача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.");
 
 
-
 Console.Write("Введите первую размерность массива:");
-int rows=Convert.ToInt32(Console.ReadLine());
+int i=Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите вторую размерность массива:");
-int columns=Convert.ToInt32(Console.ReadLine());
+int j=Convert.ToInt32(Console.ReadLine());
 
-int [,] matrix=new int [rows,columns];
+int [,] matrix=new int [i,j];
 
 Console.WriteLine("Сформированный массив:");
-for(int i=0; i<matrix.GetLength(0); i++)
+for(i=0; i<matrix.GetLength(0); i++)
 {
-    for(int j=0; j<matrix.GetLength(1); j++)
+    for(j=0; j<matrix.GetLength(1); j++)
     {
     matrix[i,j]= new Random().Next(0,10);
      Console.Write("{0:D2} ", matrix[i,j]);
@@ -22,7 +20,12 @@ for(int i=0; i<matrix.GetLength(0); i++)
     }
     Console.WriteLine();
 }
+Console.Write("Введите номер строки элемента:");
+i=Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите номер колонки элемента:");
-int rows=Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите вторую размерность массива:");
-int columns=Convert.ToInt32(Console.ReadLine());
+j=Convert.ToInt32(Console.ReadLine());
+if(i>0 & i<=matrix.GetLength(0) & j>0 & j<=matrix.GetLength(1))
+{
+  Console.WriteLine("Значение элемента позицией {0},{1}: {2:D2}",i,j,matrix[i-1,j-1]);
+}
+else{Console.WriteLine($"Елемента с позицией {i};{j} нет");}
